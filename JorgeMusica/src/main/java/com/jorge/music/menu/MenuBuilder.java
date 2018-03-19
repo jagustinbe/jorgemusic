7558806ec07ebdd21b9553e0e983363b20136352
jagustinbe <j_agustinbe@hotmail.com>
2018-03-19 01:52:09 +0000

    Removed unimplemented options in the menu

--- JorgeMusica/src/main/java/com/jorge/music/menu/MenuBuilder.java
+++ JorgeMusica/src/main/java/com/jorge/music/menu/MenuBuilder.java
@@ -40,17 +40,11 @@ public class MenuBuilder {
 		MenuHeading playHeading = 
 			new MenuHeading (1,"Review","Music and Life playlist review");
 		
-		MenuHeading maintainHeading = 
-				new MenuHeading (2,"Maintenance","Add users, playlists and music");		
-			
-		MenuHeading adminHeading = 
-				new MenuHeading (3,"Administration","Music and Life program administration options");
 		
 		// Add headings to the menu
 		
 		aMenu.add(playHeading);
-		aMenu.add(maintainHeading);
-		aMenu.add(adminHeading);
+		
 		
 		// play menu
 		
@@ -74,66 +68,6 @@ public class MenuBuilder {
 				"List the users in the system",
 				"com.jorge.music.user.UserViewMenuItem"
 				));	
-		
-		// maintain menu
-		
-		maintainHeading.add( new MenuItem( 
-				1,
-				"Add User",
-				"Add a user",
-				"com.jorge.music.menu.UndefinedMenuItem"
-				));		
-		
-		maintainHeading.add( new MenuItem( 
-				2,
-				"Add Music",
-				"Add music",
-				"com.jorge.music.menu.UndefinedMenuItem"
-				));			
-
-		maintainHeading.add( new MenuItem( 
-				3,
-				"Add Playlist",
-				"Add a playlist",
-				"com.jorge.music.menu.UndefinedMenuItem"
-				));	
-		
-		maintainHeading.add( new MenuItem( 
-				4,
-				"Edit Music",
-				"Edit music",
-				"com.jorge.music.menu.UndefinedMenuItem"
-				));	
-		
-		maintainHeading.add( new MenuItem( 
-				5,
-				"Edit Playlist",
-				"Edit a playlist",
-				"com.jorge.music.menu.UndefinedMenuItem"
-				));
-
-		// admin menu
-		
-		adminHeading.add( new MenuItem( 
-				1,
-				"Remove a User",
-				"Remove a user",
-				"com.jorge.music.menu.UndefinedMenuItem"
-				));
-		
-		adminHeading.add( new MenuItem( 
-				2,
-				"Remove Music",
-				"Remove music",
-				"com.jorge.music.menu.UndefinedMenuItem"
-				));
-		
-		adminHeading.add( new MenuItem( 
-				3,
-				"Remove a Playlist",
-				"Remove a playlist",
-				"com.jorge.music.menu.UndefinedMenuItem"
-				));
 	}
 	
 	public Menu getMenu()
